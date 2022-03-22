11672b8bb0672ff550a58d43598a38f9cfb94d58
ROBERT WEBBER - STUDENT <G00365406@gmit.ie>
2022-03-22 10:32:14 +0000

    changes to part 2

--- src/main/java/ie/gmit/open/CalculateBill.java
+++ src/main/java/ie/gmit/open/CalculateBill.java
@@ -1,2 +1,13 @@
-package ie.gmit.open;public class CalculateBill {
+package ie.gmit.open;
+
+import java.util.List;
+
+public class CalculateBill extends Subscriber{
+
+    public double calculateBill() {
+        List<CallHistory.Call> sessions = CallHistory.getCurrentCalls(getSubscriberId());
+        long totalDuration = sessions.stream().mapToLong(CallHistory.Call::getDuration).sum();
+        return totalDuration*getBaseRate()/100;
+    }
+
 }
--- src/main/java/ie/gmit/open/CallHistory.java
+++ src/main/java/ie/gmit/open/CallHistory.java
@@ -7,7 +7,7 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
-public class CallHistory {
+public class CallHistory extends Subscriber{
 
     public static class Call {
         
